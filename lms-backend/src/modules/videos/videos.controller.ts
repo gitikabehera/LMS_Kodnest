458e4ea0c1289e7daaf735865ed8237c5e5e3066
gitikabehera <gitikabehera@gmail.com>
2026-03-19 10:18:08 +0530

    fix: move @types to dependencies for Render production build

--- lms-backend/src/modules/videos/videos.controller.ts
+++ lms-backend/src/modules/videos/videos.controller.ts
@@ -1,9 +1,12 @@
-import { Response, NextFunction } from 'express';
-import { AuthRequest } from '../../middleware/authenticate';
+import { Request, Response, NextFunction } from 'express';
 import * as svc from './videos.service';
+
+interface AuthRequest extends Request {
+  userId?: number;
+}
 
 export async function getVideo(req: AuthRequest, res: Response, next: NextFunction) {
   try {
-    res.json(await svc.getVideoById(Number(req.params.id), req.userId!));
+    res.json(await svc.getVideoById(Number(req.params['id']), req.userId!));
   } catch (err) { next(err); }
 }
